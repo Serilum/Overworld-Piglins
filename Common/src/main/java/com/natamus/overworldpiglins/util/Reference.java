7a53d64b4311be346aab3fc53c0431e9d905592a
Rick South <rick@natamus.com>
2023-12-12 00:37:57 +0100

    Overworld Piglins source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/overworldpiglins/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.overworldpiglins.util;
+
+public class Reference {
+	public static final String MOD_ID = "overworldpiglins";
+	public static final String NAME = "Overworld Piglins";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
+}
