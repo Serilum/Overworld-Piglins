586a708a7fe4333d6b986802dec7dd59fa93903f
Rick South <rick@natamus.com>
2023-12-12 00:38:20 +0100

    Overworld Piglins source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/overworldpiglins/neoforge/mixin/HoglinMixin.java
@@ -0,0 +1,23 @@
+package com.natamus.overworldpiglins.neoforge.mixin;
+
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Hoglin.class, priority = 1001)
+public class HoglinMixin {
+	@Inject(method = "isImmuneToZombification()Z", at = @At(value = "RETURN"), cancellable = true)
+	protected void isImmuneToZombification(CallbackInfoReturnable<Boolean> cir) {
+		Hoglin hoglin = (Hoglin)(Object)this;
+		if (hoglin.hasEffect(MobEffects.WEAKNESS)) {
+			hoglin.setTicksFrozen(Integer.MAX_VALUE);
+			cir.setReturnValue(false);
+		}
+		else {
+			cir.setReturnValue(true);
+		}
+	}
+}
