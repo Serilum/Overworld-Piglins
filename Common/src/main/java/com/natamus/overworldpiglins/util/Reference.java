872b18ec650801dc59d7dbe1915f24fbadf876bb
Rick South <rick@natamus.com>
2024-07-28 07:22:18 +0200

    (v1.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/overworldpiglins/util/Reference.java
+++ Common/src/main/java/com/natamus/overworldpiglins/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.overworldpiglins.util;
 public class Reference {
 	public static final String MOD_ID = "overworldpiglins";
 	public static final String NAME = "Overworld Piglins";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
