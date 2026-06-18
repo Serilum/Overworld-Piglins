3afa69d923c7e8891dff07ccb38bf9a39b3625c9
Rick South <rick@natamus.com>
2026-06-18 23:34:59 +0200

    Updated Overworld Piglins to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/overworldpiglins/util/Reference.java
+++ Common/src/main/java/com/natamus/overworldpiglins/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "overworldpiglins";
 	public static final String NAME = "Overworld Piglins";
 	public static final String VERSION = "1.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
